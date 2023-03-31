d8e138ccbb81c1e2f98bc2e38b13adc4ada06177
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2023-03-31 08:56:06 +0300

    scroll ProfileScreen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, createContext, useEffect } from "react";
 import { TouchableOpacity, Image, StyleSheet } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -8,157 +8,149 @@ import CreatePostsScreen from "../CreatePostsScreen/CreatePostsScreen";
 import PostsScreen from "../PostsScreen/PostsScreen";
 import ProfileScreen from "../ProfileScreen/ProfileScreen";
 import { IsAuthContext } from "../../../App";
-import { ScrollPositionContext } from "../../screens/ProfileScreen/ProfileScreen";
 
 const Tabs = createBottomTabNavigator();
+export const IsScrollPositionContext = createContext(0);
 
 export default Home = ({ navigation }) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
   const { isAuth, setIsAuth } = useContext(IsAuthContext);
-  // const scrollPosition = useContext(ScrollPositionContext);
-  // const [position, setPosition] = useState(scrollPosition);
-  // // console.log("ScrollPositionContext", ScrollPositionContext._currentValue);
-  // console.log("scrollPosition", scrollPosition);
-  // // let position = ScrollPositionContext._currentValue;
-  // console.log("position", position);
-  // useEffect(() => {
-  //   // код для виконання при зміні компонента
-  //   console.log("scrollPosition", scrollPosition);
 
-  //   return () => {
-  //     // код для очищення після зміни компонента
-  //   };
-  // }, [ScrollPositionContext, scrollPosition]);
   return (
-    <Tabs.Navigator
-      screenOptions={({ route }) => ({
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: "#212121",
-        tabBarInactiveTintColor: "#212121",
-        // tabBarActiveBackgroundColor: "#fff",
-        // tabBarInactiveBackgroundColor: "#ff0",
-        tabBarPressOpacity: 0.2,
-        tabBarScrollEnabled: true,
-        tabBarStyle: {
-          height: 83,
-          paddingLeft: "12%",
-          paddingRight: "12%",
-          // display: "flex",
-        },
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          let styleName;
-
-          if (route.name === "PostsScreen") {
-            iconName = focused ? "grid-outline" : "grid-outline";
-            // console.log(route);
-          }
-          if (route.name === "CreatePostsScreen") {
-            iconName = focused ? "person-outline" : "add";
-            styleName = styles.centerBtn;
-          }
-          if (route.name === "ProfileScreen") {
-            iconName = focused ? "add" : "person-outline";
-          }
-
-          return (
-            <Ionicons
-              name={iconName}
-              size={size}
-              color={color}
-              style={styleName}
-            />
-          );
-        },
-      })}
+    <IsScrollPositionContext.Provider
+      value={{ scrollPosition, setScrollPosition }}
     >
-      <Tabs.Screen
-        name="PostsScreen"
-        component={PostsScreen}
-        options={({ route }) => ({
-          scrollEnabled: true,
-          title: "Публікації",
-          headerStyle: {
-            backgroundColor: "#fff",
-            height: 88,
-            elevation: 1,
-            shadowColor: "#000",
-            shadowOffset: { width: 0, height: 1 },
-            shadowOpacity: 0.2,
-            shadowRadius: 2,
-          },
-          headerTintColor: "#212121",
-          headerTitleStyle: {
-            fontFamily: "Roboto-Medium",
-            fontSize: 17,
-            textAlign: "center",
-          },
-          headerTitleAlign: "center",
-
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => setIsAuth(null)}
-              activeOpacity={0.3}
-              style={{ marginRight: 19 }}
-            >
-              <Image
-                source={require("../../images/log-out.png")}
-                style={{ width: 24, height: 24 }}
-                tintColor="#bdbdbd"
-              />
-            </TouchableOpacity>
-          ),
-        })}
-      />
-      <Tabs.Screen
-        name="CreatePostsScreen"
-        component={CreatePostsScreen}
-        options={({ route }) => ({
+      <Tabs.Navigator
+        screenOptions={({ route }) => ({
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#212121",
+          tabBarInactiveTintColor: "#212121",
+          // tabBarActiveBackgroundColor: "#fff",
+          // tabBarInactiveBackgroundColor: "#ff0",
+          tabBarPressOpacity: 0.2,
+          tabBarScrollEnabled: true,
           tabBarStyle: {
-            display: "none",
+            height: 83,
+            paddingLeft: "12%",
+            paddingRight: "12%",
+            // display: "flex",
           },
-          title: "Створити публікацію",
-          headerStyle: {
-            backgroundColor: "#fff",
-            height: 88,
-            elevation: 1, // властивість для підняття елемента і створення тіні
-            shadowColor: "#000", // колір тіні
-            shadowOffset: { width: 0, height: 1 }, // зміщення тіні відносно елемента
-            shadowOpacity: 0.2, // прозорість тіні
-            shadowRadius: 2, // радіус тіні
-          },
-          headerTintColor: "#212121",
-          headerTitleStyle: {
-            fontFamily: "Roboto-Regular",
-            fontSize: 17,
-            textAlign: "center",
-          },
-          headerTitleAlign: "center",
-          headerLeft: () => (
-            <TouchableOpacity>
-              <Ionicons
-                name="arrow-back"
-                size={24}
-                color="#212121"
-                style={{ marginLeft: 20 }}
-                onPress={() => navigation.goBack()}
-              />
-            </TouchableOpacity>
-          ),
-        })}
-      />
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let styleName;
 
-      <Tabs.Screen
-        name="ProfileScreen"
-        component={ProfileScreen}
-        options={{
-          // tabBarIcon: ({ focused, size, color, opacity }) => (
-          //   <Ionicons name="person-outline" size={size} color={color} />
-          // ),
-          headerShown: false,
-          tabBarStyle: { transform: [{ translateY: 0 }] },
-        }}
-      ></Tabs.Screen>
-    </Tabs.Navigator>
+            if (route.name === "PostsScreen") {
+              iconName = focused ? "grid-outline" : "grid-outline";
+              // console.log(route);
+            }
+            if (route.name === "CreatePostsScreen") {
+              iconName = focused ? "person-outline" : "add";
+              styleName = styles.centerBtn;
+            }
+            if (route.name === "ProfileScreen") {
+              iconName = focused ? "add" : "person-outline";
+            }
+
+            return (
+              <Ionicons
+                name={iconName}
+                size={size}
+                color={color}
+                style={styleName}
+              />
+            );
+          },
+        })}
+      >
+        <Tabs.Screen
+          name="PostsScreen"
+          component={PostsScreen}
+          options={({ route }) => ({
+            scrollEnabled: true,
+            title: "Публікації",
+            headerStyle: {
+              backgroundColor: "#fff",
+              height: 88,
+              elevation: 1,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 1 },
+              shadowOpacity: 0.2,
+              shadowRadius: 2,
+            },
+            headerTintColor: "#212121",
+            headerTitleStyle: {
+              fontFamily: "Roboto-Medium",
+              fontSize: 17,
+              textAlign: "center",
+            },
+            headerTitleAlign: "center",
+
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => setIsAuth(null)}
+                activeOpacity={0.3}
+                style={{ marginRight: 19 }}
+              >
+                <Image
+                  source={require("../../images/log-out.png")}
+                  style={{ width: 24, height: 24 }}
+                  tintColor="#bdbdbd"
+                />
+              </TouchableOpacity>
+            ),
+          })}
+        />
+        <Tabs.Screen
+          name="CreatePostsScreen"
+          component={CreatePostsScreen}
+          options={({ route }) => ({
+            tabBarStyle: {
+              display: "none",
+            },
+            title: "Створити публікацію",
+            headerStyle: {
+              backgroundColor: "#fff",
+              height: 88,
+              elevation: 1, // властивість для підняття елемента і створення тіні
+              shadowColor: "#000", // колір тіні
+              shadowOffset: { width: 0, height: 1 }, // зміщення тіні відносно елемента
+              shadowOpacity: 0.2, // прозорість тіні
+              shadowRadius: 2, // радіус тіні
+            },
+            headerTintColor: "#212121",
+            headerTitleStyle: {
+              fontFamily: "Roboto-Regular",
+              fontSize: 17,
+              textAlign: "center",
+            },
+            headerTitleAlign: "center",
+            headerLeft: () => (
+              <TouchableOpacity>
+                <Ionicons
+                  name="arrow-back"
+                  size={24}
+                  color="#212121"
+                  style={{ marginLeft: 20 }}
+                  onPress={() => navigation.goBack()}
+                />
+              </TouchableOpacity>
+            ),
+          })}
+        />
+
+        <Tabs.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
+          options={{
+            // tabBarIcon: ({ focused, size, color, opacity }) => (
+            //   <Ionicons name="person-outline" size={size} color={color} />
+            // ),
+            headerShown: false,
+            tabBarStyle: { transform: [{ translateY: -scrollPosition }] },
+          }}
+        ></Tabs.Screen>
+      </Tabs.Navigator>
+    </IsScrollPositionContext.Provider>
   );
 };
 
--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -17,67 +17,67 @@ import {
 
 import { DATA } from "../../data";
 import { styles } from "./ProfileStyle";
-
-export const ScrollPositionContext = createContext(0);
+import { IsScrollPositionContext } from "../Home/Home";
 
 export default ProfileScreen = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
+  const { scrollPosition, setScrollPosition } = useContext(
+    IsScrollPositionContext
+  );
   const handleScroll = (event) => {
     const { y } = event.nativeEvent.contentOffset;
     setScrollPosition(y);
   };
-  console.log(scrollPosition);
+  // console.log(scrollPosition);
   return (
+    // <ScrollView style={{ flex: 1 }}>
     <ImageBackground
       style={styles.image}
       source={require("../../images/BG.jpg")}
     >
-      <ScrollPositionContext.Provider value={{ scrollPosition }}>
-        <View style={styles.container}>
-          <View style={styles.whiteBox}>
-            <FlatList
-              style={{ width: "100%", flex: 0.5 }}
-              onScroll={handleScroll}
-              scrollEventThrottle={16}
-              data={DATA}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) => (
-                <View style={styles.postContainer}>
-                  <Image source={item.imageUrl} style={styles.postImage} />
-                  <View style={styles.postTitleContainer}>
-                    <Text style={styles.postTitle}>{item.title}</Text>
+      <View style={styles.container}>
+        <View style={styles.whiteBox}>
+          <FlatList
+            style={{ width: "100%", flex: 0.5 }}
+            onScroll={handleScroll}
+            scrollEventThrottle={1}
+            data={DATA}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.postContainer}>
+                <Image source={item.imageUrl} style={styles.postImage} />
+                <View style={styles.postTitleContainer}>
+                  <Text style={styles.postTitle}>{item.title}</Text>
+                </View>
+                <View style={styles.postInfoContainer}>
+                  <View style={styles.postDataContainers}>
+                    <Image
+                      source={require("../../images/Shape.png")}
+                      style={styles.postIcon}
+                    />
+                    <Text
+                      style={{
+                        ...styles.postText,
+                        color: "#BDBDBD",
+                        textDecorationLine: "none",
+                      }}
+                    >
+                      {item.commentsCounter}
+                    </Text>
                   </View>
-                  <View style={styles.postInfoContainer}>
-                    <View style={styles.postDataContainers}>
-                      <Image
-                        source={require("../../images/Shape.png")}
-                        style={styles.postIcon}
-                      />
-                      <Text
-                        style={{
-                          ...styles.postText,
-                          color: "#BDBDBD",
-                          textDecorationLine: "none",
-                        }}
-                      >
-                        {item.commentsCounter}
-                      </Text>
-                    </View>
-                    <View style={styles.postDataContainers}>
-                      <Image
-                        source={require("../../images/map-pin.png")}
-                        style={styles.postIcon}
-                      />
-                      <Text style={styles.postText}>{item.location}</Text>
-                    </View>
+                  <View style={styles.postDataContainers}>
+                    <Image
+                      source={require("../../images/map-pin.png")}
+                      style={styles.postIcon}
+                    />
+                    <Text style={styles.postText}>{item.location}</Text>
                   </View>
                 </View>
-              )}
-            />
-          </View>
+              </View>
+            )}
+          />
         </View>
-      </ScrollPositionContext.Provider>
+      </View>
     </ImageBackground>
+    // </ScrollView>
   );
 };
